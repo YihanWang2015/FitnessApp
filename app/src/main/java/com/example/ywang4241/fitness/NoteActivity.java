481ea50ba2190791a52a5f66d491454658cd4b00
YihanWang2015 <ywang4241@mail.kvcc.edu>
2015-12-01 21:34:35 -0500

    Create data management layer

--- app/src/main/java/com/example/ywang4241/fitness/NoteActivity.java
+++ app/src/main/java/com/example/ywang4241/fitness/NoteActivity.java
@@ -1,10 +1,74 @@
 package com.example.ywang4241.fitness;
 
+import android.app.LoaderManager;
+import android.content.ContentValues;
+import android.content.CursorLoader;
+import android.content.Loader;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.CursorAdapter;
+import android.widget.ListView;
+import android.widget.SimpleCursorAdapter;
+
 /**
  * Created by bigy on 12/1/15.
  */
-public class NoteActivity {
+public class NoteActivity extends AppCompatActivity
+implements LoaderManager.LoaderCallbacks<Cursor>
 
+{
+    private CursorAdapter cursorAdapter;
 
+   // @TargetApi(Build.VERSION_CODES.JELLY_BEAN)
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_note);
 
+       /* DBOpenHelper helper = new DBOpenHelper(this);
+        SQLiteDatabase database = helper.getWritableDatabase();*/
+
+        insertNote("New note");
+
+       /* Cursor cursor = getContentResolver().query(NoteProvider.CONTENT_URI,
+                DBOpenHelper.ALL_COLUMNS, null, null, null, null);*/
+
+        String[] from = {DBOpenHelper.NOTE_TEXT};
+        int[] to ={android.R.id.text1};
+
+        cursorAdapter = new SimpleCursorAdapter(this,
+                android.R.layout.simple_list_item_1, null, from, to, 0);
+
+        ListView list = (ListView) findViewById(android.R.id.list);
+        list.setAdapter(cursorAdapter);
+
+        getLoaderManager().initLoader(0, null, this);
+
+    }
+
+    private void insertNote(String noteText) {
+        ContentValues values = new ContentValues();
+        values.put(DBOpenHelper.NOTE_TEXT, "New note");
+        Uri noteUri = getContentResolver().insert(NoteProvider.CONTENT_URI, values);
+
+        Log.d("NoteActivity", "INserted note " + noteUri.getLastPathSegment());
+    }
+
+    @Override
+    public Loader<Cursor> onCreateLoader(int id, Bundle args) {
+        return new CursorLoader(this, NoteProvider.CONTENT_URI, null, null, null, null);
+    }
+
+    @Override
+    public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
+        cursorAdapter.swapCursor(data);
+    }
+
+    @Override
+    public void onLoaderReset(Loader<Cursor> loader) {
+        cursorAdapter.swapCursor(null);
+    }
 }
