1be8cf29e43f1e3804a6d4973798f1a6b84b927f
aaerniestudent <aaernie7528@mail.kvcc.edu>
2015-12-21 11:08:41 -0500

    Add to goal control, add pedometer fragment with minor work.

--- app/src/main/java/com/example/ywang4241/fitness/Goal.java
+++ app/src/main/java/com/example/ywang4241/fitness/Goal.java
@@ -1,11 +1,25 @@
 package com.example.ywang4241.fitness;
 
+import java.util.Date;
+import java.util.UUID;
+
 /**
  * Created by ywang4241 on 11/13/2015.
  */
 public class Goal {
 
+    private UUID mId;
     private String mGoal;
+    private Date mDate;
+    private int mSteps;
+
+    public Goal(){
+        //generate unique identifier
+        mId = UUID.randomUUID();
+        mDate = new Date();
+        mSteps = 0;
+    }
+
 
     public String getGoal() {
         return mGoal;
@@ -13,5 +27,29 @@ public class Goal {
 
     public void setGoal(String goal) {
         this.mGoal = goal;
+    }
+
+    public String getmGoal() {
+        return mGoal;
+    }
+
+    public void setmGoal(String mGoal) {
+        this.mGoal = mGoal;
+    }
+
+    public Date getmDate() {
+        return mDate;
+    }
+
+    public void setmDate(Date mDate) {
+        this.mDate = mDate;
+    }
+
+    public int getmSteps() {
+        return mSteps;
+    }
+
+    public void setmSteps(int mSteps) {
+        this.mSteps = mSteps;
     }
 }
